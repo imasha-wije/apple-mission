61981c52a836ff2fcd0ef732d9186085219280df
imasha-wije <118158951+imasha-wije@users.noreply.github.com>
2022-11-13 23:05:59 -0800

    Add files via upload

---
+++ docs/Gallery/articles.php
@@ -0,0 +1,170 @@
+<?php include('../header.php')?>
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+	
+
+	<!-- END #fh5co-header --><br><br>
+	<div class="container-fluid">
+		<div class="row fh5co-post-entry">
+			<article class="col-lg-3 col-md-3 col-sm-3 col-xs-6 col-xxs-12 animate-box">
+				<figure>
+					<img src="images/patient.png" alt="Image"  id="img-responsive-custom" width="100%" height="300%">
+				</figure>
+				<h2 class="fh5co-article-title"><b>Mental Patient</b></h2>
+				<span class="fh5co-meta"><font color="#666666">There is probably no one who has not
+                    heard the word "crazy", </span></font>
+					<p><a href="pdf/mental patient.pdf" target=_blank class="btn btn-primary" data-lightbox="image-1"> <font color="blue"face="times new roman">Read more</font></a></p>
+			</article>
+			
+			<article class="col-lg-3 col-md-3 col-sm-3 col-xs-6 col-xxs-12 animate-box">
+				<figure>
+					<img src="images/rela.jpg" alt="Image"  id="img-responsive-custom"width="100%" height="400%">
+				</figure>
+				<h2 class="fh5co-article-title"><b>Relationship and Mental Health</b></h2>
+				<span class="fh5co-meta"><font color="#666666">How can I willingly learn
+                   a subject that I don't like?</span></font>
+				<p><a href="pdf/Relationship & Mental Health.pdf" target=_blank class="btn btn-primary" data-lightbox="image-1"><font color="blue"face="times new roman">Read more</font></a></p>
+			</article>
+			<div class="clearfix visible-xs-block"></div>
+			<article class="col-lg-3 col-md-3 col-sm-3 col-xs-6 col-xxs-12 animate-box">
+				<figure>
+					<a href="single.html"><img src="images/pic_3.jpg" alt="Image"  id="img-responsive-custom"></a>
+				</figure>
+				<span class="fh5co-meta"><a href="single.html">Travel</a>, <a href="single.html">Style</a></span>
+				<h2 class="fh5co-article-title"><a href="single.html">An Overlooking River at the East Cost</a></h2>
+				<span class="fh5co-meta fh5co-date">March 6th, 2016</span>
+			</article>
+			<article class="col-lg-3 col-md-3 col-sm-3 col-xs-6 col-xxs-12 animate-box">
+				<figure>
+					<a href="single.html"><img src="images/pic_4.jpg" alt="Image"  id="img-responsive-custom"></a>
+				</figure>
+				<span class="fh5co-meta"><a href="single.html">Travel</a>, <a href="single.html">Style</a></span>
+				<h2 class="fh5co-article-title"><a href="single.html">A Wildlife In The Mountain of India</a></h2>
+				<span class="fh5co-meta fh5co-date">March 6th, 2016</span>
+			</article>
+			<div class="clearfix visible-lg-block visible-md-block visible-sm-block visible-xs-block"></div>
+			<article class="col-lg-3 col-md-3 col-sm-3 col-xs-6 col-xxs-12 animate-box">
+			
+			
+				<figure>
+					<a href="single.html"><img src="images/pic_5.jpg" alt="Image"  id="img-responsive-custom"></a>
+				</figure>
+				<span class="fh5co-meta"><a href="single.html">Photography</a></span>
+				<h2 class="fh5co-article-title"><a href="single.html">We Took A Photo</a></h2>
+				<span class="fh5co-meta fh5co-date">March 6th, 2016</span>
+			</article>
+			<article class="col-lg-3 col-md-3 col-sm-3 col-xs-6 col-xxs-12 animate-box">
+				<figure>
+					<a href="single.html"><img src="images/pic_6.jpg" alt="Image"  id="img-responsive-custom"></a>
+				</figure>
+				<span class="fh5co-meta"><a href="single.html">Travel</a>, <a href="single.html">Style</a></span>
+				<h2 class="fh5co-article-title"><a href="single.html">A Modernize Huge and Beautiful Building</a></h2>
+				<span class="fh5co-meta fh5co-date">March 6th, 2016</span>
+			</article>
+			<div class="clearfix visible-xs-block"></div>
+			<article class="col-lg-3 col-md-3 col-sm-3 col-xs-6 col-xxs-12 animate-box">
+				<figure>
+					<a href="single.html"><img src="images/pic_7.jpg" alt="Image"  id="img-responsive-custom"></a>
+				</figure>
+				<span class="fh5co-meta"><a href="single.html">Food &amp; Drinks</a></span>
+				<h2 class="fh5co-article-title"><a href="single.html">Enjoying the Native Juice Drink in Brazil</a></h2>
+				<span class="fh5co-meta fh5co-date">March 6th, 2016</span>
+			</article>
+			<article class="col-lg-3 col-md-3 col-sm-3 col-xs-6 col-xxs-12 animate-box">
+				<figure>
+					<a href="single.html"><img src="images/pic_8.jpg" alt="Image"  id="img-responsive-custom"></a>
+				</figure>
+				<span class="fh5co-meta"><a href="single.html">Travel</a>, <a href="single.html">Style</a></span>
+				<h2 class="fh5co-article-title"><a href="single.html">Boat Travel in The Vietnam River</a></h2>
+				<span class="fh5co-meta fh5co-date">March 6th, 2016</span>
+			</article>
+			
+			
+			<div class="clearfix visible-lg-block visible-md-block visible-sm-block visible-xs-block"></div>
+			<article class="col-lg-3 col-md-3 col-sm-3 col-xs-6 col-xxs-12 animate-box">
+				<figure>
+					<a href="single.html"><img src="images/pic_1.jpg" alt="Image"  id="img-responsive-custom"></a>
+				</figure>
+				<span class="fh5co-meta"><a href="single.html">Food &amp; Drink</a></span>
+				<h2 class="fh5co-article-title"><a href="single.html">We Eat and Drink All Night</a></h2>
+				<span class="fh5co-meta fh5co-date">March 6th, 2016</span>
+			</article>
+			<article class="col-lg-3 col-md-3 col-sm-3 col-xs-6 col-xxs-12 animate-box">
+				<figure>
+					<a href="single.html"><img src="images/pic_2.jpg" alt="Image"  id="img-responsive-custom"></a>
+				</figure>
+				<span class="fh5co-meta"><a href="single.html">Food &amp; Drink</a></span>
+				<h2 class="fh5co-article-title"><a href="single.html">Beef Steak at Guatian Restaurant</a></h2>
+				<span class="fh5co-meta fh5co-date">March 6th, 2016</span>
+			</article>
+			<div class="clearfix visible-xs-block"></div>
+			<article class="col-lg-3 col-md-3 col-sm-3 col-xs-6 col-xxs-12 animate-box">
+				<figure>
+					<a href="single.html"><img src="images/pic_3.jpg" alt="Image"  id="img-responsive-custom"></a>
+				</figure>
+				<span class="fh5co-meta"><a href="single.html">Travel</a>, <a href="single.html">Style</a></span>
+				<h2 class="fh5co-article-title"><a href="single.html">An Overlooking River at the East Cost</a></h2>
+				<span class="fh5co-meta fh5co-date">March 6th, 2016</span>
+			</article>
+			<article class="col-lg-3 col-md-3 col-sm-3 col-xs-6 col-xxs-12 animate-box">
+				<figure>
+					<a href="single.html"><img src="images/pic_4.jpg" alt="Image"  id="img-responsive-custom"></a>
+				</figure>
+				<span class="fh5co-meta"><a href="single.html">Travel</a>, <a href="single.html">Style</a></span>
+				<h2 class="fh5co-article-title"><a href="single.html">A Wildlife In The Mountain of India</a></h2>
+				<span class="fh5co-meta fh5co-date">March 6th, 2016</span>
+			</article>
+			
+			
+			<div class="clearfix visible-lg-block visible-md-block visible-sm-block visible-xs-block"></div>
+			<article class="col-lg-3 col-md-3 col-sm-3 col-xs-6 col-xxs-12 animate-box">
+				<figure>
+					<a href="single.html"><img src="images/pic_5.jpg" alt="Image" id="img-responsive-custom"></a>
+				</figure>
+				<span class="fh5co-meta"><a href="single.html">Photography</a></span>
+				<h2 class="fh5co-article-title"><a href="single.html">We Took A Photo</a></h2>
+				<span class="fh5co-meta fh5co-date">March 6th, 2016</span>
+			</article>
+			<article class="col-lg-3 col-md-3 col-sm-3 col-xs-6 col-xxs-12 animate-box">
+				<figure>
+					<a href="single.html"><img src="images/pic_6.jpg" alt="Image"  id="img-responsive-custom"></a>
+				</figure>
+				<span class="fh5co-meta"><a href="single.html">Travel</a>, <a href="single.html">Style</a></span>
+				<h2 class="fh5co-article-title"><a href="single.html">A Modernize Huge and Beautiful Building</a></h2>
+				<span class="fh5co-meta fh5co-date">March 6th, 2016</span>
+			</article>
+			<div class="clearfix visible-xs-block"></div>
+			<article class="col-lg-3 col-md-3 col-sm-3 col-xs-6 col-xxs-12 animate-box">
+				<figure>
+					<a href="single.html"><img src="images/pic_7.jpg" alt="Image"  id="img-responsive-custom"></a>
+				</figure>
+				<span class="fh5co-meta"><a href="single.html">Food &amp; Drinks</a></span>
+				<h2 class="fh5co-article-title"><a href="single.html">Enjoying the Native Juice Drink in Brazil</a></h2>
+				<span class="fh5co-meta fh5co-date">March 6th, 2016</span>
+			</article>
+			<article class="col-lg-3 col-md-3 col-sm-3 col-xs-6 col-xxs-12 animate-box">
+				<figure>
+					<a href="single.html"><img src="images/pic_8.jpg" alt="Image"  id="img-responsive-custom"></a>
+				</figure>
+				<span class="fh5co-meta"><a href="single.html">Travel</a>, <a href="single.html">Style</a></span>
+				<h2 class="fh5co-article-title"><a href="single.html">Boat Travel in The Vietnam River</a></h2>
+				<span class="fh5co-meta fh5co-date">March 6th, 2016</span>
+			</article>
+			<div class="clearfix visible-xs-block"></div>
+		</div>
+	</div>
+
+	<footer id="fh5co-footer">
+		<p><small>&copy; 2016. Magazine Free HTML5. All Rights Reserverd. <br> Designed by <a href="http://freehtml5.co" target="_blank">FREEHTML5.co</a>  Demo Images: <a href="http://unsplash.com/" target="_blank">Unsplash</a></small></p>
+	</footer>
+
+
+	
+	
+
+	</body>
+</html>
+
